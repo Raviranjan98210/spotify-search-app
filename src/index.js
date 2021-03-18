cbaedbbe5b44661db991574851ab55252d9f16dc
Ravi Ranjan <raviranjan98210@gmail.com>
2021-03-18 17:42:21 +0530

    Added authorization

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
+import { Provider } from "react-redux";
+import store from "./store/store";
+import AppRouter from "./router/AppRouter";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
   document.getElementById("root")
 );
