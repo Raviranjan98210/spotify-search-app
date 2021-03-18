95a13b593a4500471155659f667c09d665f2e163
Ravi Ranjan <raviranjan98210@gmail.com>
2021-03-18 18:23:26 +0530

    Added album search

--- src/actions/result.js
+++ src/actions/result.js
@@ -7,30 +7,37 @@ import {
   ADD_PLAYLIST,
 } from "../utils/constants";
 import { get } from "../utils/api";
+
 export const setAlbums = albums => ({
   type: SET_ALBUMS,
   albums,
 });
+
 export const addAlbums = albums => ({
   type: ADD_ALBUMS,
   albums,
 });
+
 export const setArtists = artists => ({
   type: SET_ARTISTS,
   artists,
 });
+
 export const addArtists = artists => ({
   type: ADD_ARTISTS,
   artists,
 });
+
 export const setPlayList = playlists => ({
   type: SET_PLAYLIST,
   playlists,
 });
+
 export const addPlaylist = playlists => ({
   type: ADD_PLAYLIST,
   playlists,
 });
+
 export const initiateGetResult = searchTerm => {
   return async dispatch => {
     try {
@@ -43,6 +50,39 @@ export const initiateGetResult = searchTerm => {
       dispatch(setAlbums(albums));
       dispatch(setArtists(artists));
       return dispatch(setPlayList(playlists));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+};
+
+export const initiateLoadMoreAlbums = url => {
+  return async dispatch => {
+    try {
+      const result = await get(url);
+      return dispatch(addAlbums(result.albums));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+};
+
+export const initiateLoadMoreArtists = url => {
+  return async dispatch => {
+    try {
+      const result = await get(url);
+      return dispatch(addArtists(result.artists));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+};
+
+export const initiateLoadMorePlaylist = url => {
+  return async dispatch => {
+    try {
+      const result = await get(url);
+      return dispatch(addPlaylist(result.playlists));
     } catch (error) {
       console.log("error", error);
     }
